cd79b6264820c49e895dc9d00f466b1a994f082d
Berachem Markria <61350744+Berachem@users.noreply.github.com>
2024-01-28 21:01:16 +0100

    Update Calcul Stratégie Martingale.py

--- Calcul Stratégie Martingale.py
+++ Calcul Stratégie Martingale.py
@@ -1,26 +1,26 @@
-def martingale(**kwargs):
+def martingale(s_dep=None, s_totale=None, marge=None):
     """
-    Fonction qui conseil pour la stratégie de la Martingale :
+    Fonction qui conseille pour la stratégie de la Martingale :
         PARAMETRE:
         - s_dep OU s_totale
-        - marge (marge qu'on s'autorise à loose)
+        - marge (marge qu'on s'autorise à perdre)
         RENVOIE :
         - S_dep : la somme de départ à déposer (Arguments à donner : marge et s_totale)
         - S_totale : la somme totale à avoir (Arguments à donner : marge et S_dep)
-
     """
 
-    assert "marge" in kwargs.keys(), "Vous avez oubliez de mentionner la marge d'erreur !"
-    
-    if "s_dep" in kwargs.keys() and not ("s_totale" in kwargs.keys()):
-        return "La somme totale à avoir est de : " + str( kwargs["s_dep"]* sum([2**i for i in range(kwargs["marge"])]) ) + " euros."
-    
-    elif "s_totale" in kwargs.keys() and not ("s_dep" in kwargs.keys()):
-        return "La somme de départ à avoir est de : " + str( kwargs["s_totale"]/sum([2**i for i in range(kwargs["marge"])]) ) + " euros."
-    
+    # Vérification des arguments
+    assert marge is not None, "Vous avez oublié de mentionner la marge d'erreur !"
+    assert s_dep is not None or s_totale is not None, "Vous devez spécifier soit 's_dep', soit 's_totale'."
+
+    # Calcul de la somme totale ou de départ en fonction des arguments
+    if s_dep is not None and s_totale is None:
+        return f"La somme totale à avoir est de : {s_dep * sum([2**i for i in range(marge)])} euros."
+    elif s_totale is not None and s_dep is None:
+        return f"La somme de départ à avoir est de : {s_totale / sum([2**i for i in range(marge)])} euros."
     else:
-        return " [FAIL] : s_totale et s_dep ont été eux deux renseigné..."
+        return "[FAIL] : s_totale et s_dep ont été tous deux renseignés..."
 
 #EXEMPLES
-print(martingale(s_totale=400,marge=10))
-print(martingale(s_dep=0.1,marge=10))
+print(martingale(s_totale=400, marge=10))
+print(martingale(s_dep=0.1, marge=10))
